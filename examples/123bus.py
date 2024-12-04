2b86792d5f0121b935305b9d5c24cba7be59aa8d
nathantgray <nathantgray@gmail.com>
2024-12-04 13:10:00 -0800

    update 123bus.py and plot_network

--- examples/123bus.py
+++ examples/123bus.py
@@ -1,11 +1,11 @@
 import numpy as np
 import distopf as opf
-
+from distopf.lindist import LinDistModel
 case = opf.DistOPFCase(
-    data_path="ieee123", gen_mult=1, load_mult=1, v_swing=1.0, v_max=1.05, v_min=0.95
+    data_path="ieee123_30der", gen_mult=1, load_mult=1, v_swing=1.0, v_max=1.05, v_min=0.95
 )
 
-model = opf.LinDistModelQ(
+model = opf.LinDistModel(
     branch_data=case.branch_data,
     bus_data=case.bus_data,
     gen_data=case.gen_data,
@@ -13,18 +13,20 @@ model = opf.LinDistModelQ(
     reg_data=case.reg_data,
 )
 # Solve model using provided objective function
-result = opf.lp_solve(model, np.zeros(model.n_x))
+result = opf.lp_solve(model, opf.gradient_load_min(model))
 # result = cvxpy_solve(model, cp_obj_loss)
 print(result.fun)
 v = model.get_voltages(result.x)
 s = model.get_apparent_power_flows(result.x)
-dec_var = model.get_decision_variables(result.x)
-fig = opf.plot_network(model, v, s, dec_var, "Q").show(renderer="browser")
-opf.plot_voltages(v).show(renderer="browser")
-opf.plot_power_flows(s).show(renderer="browser")
+p_gens = model.get_p_gens(result.x)
+q_gens = model.get_q_gens(result.x)
+# opf.plot_network(model, v=v, s=s, p_gen=None, q_gen=q_gens).show()
+opf.plot_network(model).show()
+# opf.plot_voltages(v).show()
+# opf.plot_power_flows(s).show()
 
-fig.write_image("123_plot.pdf", format="pdf", width=1000, height=1000)
-import time
+# fig.write_image("123_plot.pdf", format="pdf", width=1000, height=1000)
+# import time
 
-time.sleep(1)
-fig.write_image("123_plot.pdf", format="pdf", width=1000, height=1000)
+# time.sleep(1)
+# fig.write_image("123_plot.pdf", format="pdf", width=1000, height=1000)
